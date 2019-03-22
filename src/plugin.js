24c0603c4ea9bdfe9b22d6c2cf5e2cf439528ce0
Liam Newmarch <liamnewmarch@users.noreply.github.com>
2019-03-22 14:31:35 +0000

    Pass component as context to render function, add comments.

--- src/plugin.js
+++ src/plugin.js
@@ -1,8 +1,34 @@
+/**
+ * Initialises the plugin with a configuration object.
+ *
+ * @param {object} config Plugin configuration object.
+ * @returns {function}
+ */
 export default ({ render }) => {
+
+  /**
+   * Creates a new plugin instance with an Acute component.
+   *
+   * @param {object} component Acute component.
+   * @returns {function}
+   */
   return (component) => {
+
+    /**
+     * Sets the component’s render strategy to a function.
+     *
+     * @param {function} fn The render function.
+     * @returns {function}
+     */
     component.renderStrategy = (fn) => {
+
+      /**
+       * Creates a template instance and uses lit-html to render the template
+       * instance to the component’s shadow root.
+       */
       return () => {
-        render(fn(component.props), component.element.shadowRoot);
+        const templateInstance = fn.call(component, component.props);
+        render(templateInstance, component.element.shadowRoot);
       };
     };
   };
