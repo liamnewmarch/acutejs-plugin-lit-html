cfef323d63421a25113bbb0d3074372a201a6f3a
Liam Newmarch <liamnewmarch@users.noreply.github.com>
2019-03-26 13:27:32 +0000

    Improvements to readability.

--- src/plugin.js
+++ src/plugin.js
@@ -4,28 +4,28 @@
  * @param {object} config Plugin configuration object.
  * @return {function}
  */
-export default ({ render }) => {
+export default function({ render }) {
   /**
    * Creates a new plugin instance with an Acute component.
    *
    * @param {object} component Acute component.
    */
-  return (component) => {
+  return function(component) {
     /**
      * Sets the component’s render strategy to a function.
      *
      * @param {function} fn The render function.
      * @return {function}
      */
-    component.renderStrategy = (fn) => {
+    component.renderStrategy = function(fn) {
       /**
        * Creates a template instance and uses lit-html to render the template
        * instance to the component’s shadow root.
        */
-      return () => {
+      return function() {
         const templateInstance = fn.call(component, component.element);
         render(templateInstance, component.element.shadowRoot);
       };
     };
   };
-};
+}
